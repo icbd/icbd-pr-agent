9a9010165ac009e523b1f641bab8919870643634
Baodong <wwwicbd@gmail.com>
2024-09-04 18:20:12 +0800

    describe MR

--- pr_agent/cli_pip.py
+++ pr_agent/cli_pip.py
@@ -1,22 +1,20 @@
+import os
+
 from pr_agent import cli
 from pr_agent.config_loader import get_settings
 
 
 def main():
-    # Fill in the following values
-    provider = "github"  # GitHub provider
-    user_token = "..."  # GitHub user token
-    openai_key = "..."  # OpenAI key
-    pr_url = "..."  # PR URL, for example 'https://github.com/Codium-ai/pr-agent/pull/809'
-    command = "/review"  # Command to run (e.g. '/review', '/describe', '/ask="What is the purpose of this PR?"')
-
     # Setting the configurations
-    get_settings().set("CONFIG.git_provider", provider)
-    get_settings().set("openai.key", openai_key)
-    get_settings().set("github.user_token", user_token)
+    get_settings().set("CONFIG.git_provider", 'gitlab')
+    get_settings().set("gitlab.url", "https://jihulab.com")
+    get_settings().set("openai.key", os.getenv('OPENAI_KEY'))
+    get_settings().set("gitlab.personal_access_token", os.getenv('PAT'))
 
-    # Run the command. Feedback will appear in GitHub PR comments
-    cli.run_command(pr_url, command)
+    cli.run_command(
+        "https://jihulab.com/cbd-premium/code-review/-/merge_requests/1",
+        "/describe"
+    )
 
 
 if __name__ == '__main__':
